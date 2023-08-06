5a9bc2ff3107fd8bcca273e8eb90523bcead6230
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-08-06 20:35:34 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Jack'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+
+}
+
+
+function overwriteBestCustomer() {
+
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Grilish';
+}
+
